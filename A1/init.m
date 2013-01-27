242ddd8d79ad020e17ec5a12bc35f873aa2d906d
Alfred Chan <alfred8844@gmail.com>
2013-01-27 17:20:49 -0500

    * updated init.m

--- A1/init.m
+++ A1/init.m
@@ -1,9 +1,14 @@
 %figure('position',get(0,'screensize'))
 %a= imread('hand.png');
-[I map] = imread('hand.png');
-imshow(I,map);
+%[I map] = imread('initial.png');
+% Load some image:
+data = imread('initial.png');
 
-[x y] = ginput;
-%figure('position',get(0,'screensize'))
-%axes('position',[0 0 1 1])
-%[x y] = ginput;
+% display the image:
+figure(1);
+clf;
+h = imagesc(data);
+axis image
+
+% Get a value from the screen:
+[x, y] = ginput;
