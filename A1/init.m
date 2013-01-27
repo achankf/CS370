ea61d4832eafe05a3b68cb3eec3d2771d91de57c
a77chan <a77chan@uwaterloo.ca>
2013-01-27 16:56:38 -0500

    * updated init.m

--- A1/init.m
+++ A1/init.m
@@ -1,6 +1,9 @@
 %figure('position',get(0,'screensize'))
 %a= imread('hand.png');
-a = image('hand.png');
+[I map] = imread('hand.png');
+imshow(I,map);
+
+[x y] = ginput;
 %figure('position',get(0,'screensize'))
 %axes('position',[0 0 1 1])
 %[x y] = ginput;
