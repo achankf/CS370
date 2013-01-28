25dc2730029d081ee90c6bdd5f3bc9a70f4e5fb9
a77chan <a77chan@uwaterloo.ca>
2013-01-28 13:42:27 -0500

    * updated data set

--- A1/m/a1q6b.m
+++ A1/m/a1q6b.m
@@ -3,10 +3,5 @@ clear
 % load data
 a1q6_data
 
-plot(x0,-y0,
-	x1,-y1,
-	x2,-y2,
-	x3,-y3,
-	x4,-y4
-);
+plot(x0,-y0, x1,-y1, x2,-y2, x3,-y3, x4,-y4, x5,-y5, x6,-y6, x7,-x7);
 title('Interpolated Data Points of My Initial Letters');
