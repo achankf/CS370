65c4c537d33b351e8e74f73b56eb154021cb866a
Alfred Chan <alfred8844@gmail.com>
2013-01-27 19:24:26 -0500

    * refinet can accept a factor * progress: a1q6

--- A1/m/refinet.m
+++ A1/m/refinet.m
@@ -1,12 +1,12 @@
-function [tref] = refinet(t)
+function [tref] = refinet(t,fac)
 N = length(t);
-tref = zeros(1, 3*(N-1) + 1);
+tref = zeros(1, fac*(N-1) + 1);
 for k = 1:N-1
-	i = 3*(k-1)+1;
+	i = fac*(k-1)+1;
 	dt = t(k+1)-t(k);
-	tref(i) = t(k);
-	tref(i+1) = t(k)+dt/3;
-	tref(i+2) = t(k)+2*dt/3;
+	for l = 0:fac-1
+		tref(i+l) = t(k) + fac*dt/fac;
+	end
 end
-tref(3*(N-1)+1) = t(N);
+tref(fac*(N-1)+1) = t(N);
 endfunction
