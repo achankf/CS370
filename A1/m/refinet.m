f34866cdea7554eb71b1b4ad972c82a9302e6183
Alfred Chan <alfred8844@gmail.com>
2013-01-28 15:49:36 -0500

    * completed a1

--- A1/m/refinet.m
+++ A1/m/refinet.m
@@ -1,12 +1,12 @@
 function [tref] = refinet(t,fac)
-N = length(t);
-tref = zeros(1, fac*(N-1) + 1);
-for k = 1:N-1
-	i = fac*(k-1)+1;
-	dt = t(k+1)-t(k);
-	for l = 0:fac-1
-		tref(i+l) = t(k) + l*dt/fac;
+	N = length(t);
+	tref = zeros(1, fac*(N-1) + 1);
+	for k = 1:N-1
+		i = fac*(k-1)+1;
+		dt = t(k+1)-t(k);
+		for l = 0:fac-1
+			tref(i+l) = t(k) + l*dt/fac;
+		end
 	end
-end
-tref(fac*(N-1)+1) = t(N);
+	tref(fac*(N-1)+1) = t(N);
 endfunction
