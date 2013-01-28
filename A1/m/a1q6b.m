4d6620ebec6a4db24b60dc6becafcb8fcb77481c
Alfred Chan <alfred8844@gmail.com>
2013-01-28 10:29:12 -0500

    * added data points for 'C' to the a1q6 data * created a1q6b.m

--- A1/m/a1q6b.m
+++ A1/m/a1q6b.m
@@ -3,11 +3,10 @@ clear
 % load data
 a1q6_data
 
-plot(
-	x0,-y0,'*',
-	x1,-y1,'*',
-	x2,-y2,'*',
-	x3,-y3,'*',
-	x4,-y4,'*'
+plot(x0,-y0,
+	x1,-y1,
+	x2,-y2,
+	x3,-y3,
+	x4,-y4
 );
 title('Interpolated Data Points of My Initial Letters');
--- A1/m/a1q6c.m
+++ A1/m/a1q6c.m
@@ -1,7 +1,7 @@
 clear
 
-type = 'not-a-knot'
-fac = 3
+type = 'not-a-knot';
+fac = 3;
 
 % load data
 a1q6_data
@@ -10,11 +10,13 @@ a1q6_data
 [tx1 ty1] = interpolate(x1,y1,type,fac);
 [tx2 ty2] = interpolate(x2,y2,type,fac);
 [tx3 ty3] = interpolate(x3,y3,type,fac);
+[tx4 ty4] = interpolate(x4,y4,type,fac);
 %[tx4 ty4] = interpolate(x4,y4,type,fac);
 
 plot(tx0,-ty0,
 	x1,-y1,
 	tx2,-ty2,
-	tx3,-ty3
+	tx3,-ty3,
+	tx4,-ty4
 );
 title('Interpolated Data Points of My Initial Letters');
