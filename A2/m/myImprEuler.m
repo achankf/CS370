8c303d61753b22ba79156af9d41b82a5860a5ce3
Alfred Chan <alfred8844@gmail.com>
2013-02-11 19:31:44 -0500

    * closer

--- A2/m/myImprEuler.m
+++ A2/m/myImprEuler.m
@@ -1,24 +1,17 @@
 function [z] = myImprEuler(t_0, t_final, N, alpha)
 	h = (t_final - t_0) / N;
+	z = zeros(1,N+1);
 	z(1) = kp = kpp = alpha;
-	kx(1) = kpx = kppx = alpha;
-	kpppx = kpx * h;
 
 	actual = zeros(1,N+1);
 	prev = 1;
 	for t = 2:N+1
-		% Forward Euler for k', k'', k'''
-		kpx_temp = kpx;
-		kpx = kp + h * kpp;
-		kppx_temp = kppx;
-		kppx = kpp + h * (kpx + prev +h);
-		kpppx = kpppx+ t* h + kppx_temp;
-
+		prev_kp = kp;
+		prev_kpp = kpp;
 		% Modified Euler for z (the result)
-		z(t) = z(prev) + h / 2 * (kp + kpx);
-		kp_temp = kp;
-		kp = kp + h / 2 * (kpp + kppx);
-		kpp = kpp + h / 2 * (kp_temp + prev * h + kpppx );
+		z(t) = z(prev) + h / 2 * (kp + (kp + h * kpp));
+		kp = kp + h / 2 * (kpp + (kpp + h * (kp+t*h)));
+		kpp = kpp + h / 2 * (kp + h*t + (kp + h*prev));
 
 		%z(t) = kx(t);
 		ss = t*h;
