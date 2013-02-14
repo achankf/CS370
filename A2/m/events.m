d5206dcaa81ff2f6fd4590412e365d8129a246da
Alfred Chan <alfred8844@gmail.com>
2013-02-14 12:30:03 -0500

    cleaning a2q4 and odefunc

--- A2/m/events.m
+++ A2/m/events.m
@@ -1,7 +1,4 @@
 function [values,halt,dir] = events(t,z)
-	%values = [z(1) z(2) z(3) z(4)];
-	%halt = [1 1 1 1];
-	%dir = [0 0 0 0];
 	values = zeros(1,1);
 	values(1) = z(2);
 	halt = zeros(1,1);
--- A2/m/odefunc.m
+++ A2/m/odefunc.m
@@ -1,17 +1,13 @@
 function dy = odefunc(t,z,p)
-	z1 = z(1);
-	z2 = z(2);
-	z3 = z(3);
-	z4 = z(4);
 	g = 9.81;
 	m = 90;
 	c = 0.72;
 	s = 0.5;
 
 	dy = zeros(4,1);
-	dy(1) = z4 + cos(z3);
-	dy(2) = z4 * sin(z3);
 	D = c * p * s / 2 * (dy(1)^2 + dy(2)^2);
-	dy(3) = -g / z4 * cos(z3);
-	dy(4) = -D / m - g * sin(z3);
+	dy = [z(4) + cos(z(3));
+		z(4) + sin(z(3));
+		-g / z(4) * cos(z(3));
+		-D / m - g * sin(z(3))];
 endfunction
