66f8de4304ea684b2918c1b72796082a687a75f9
a77chan <a77chan@uwaterloo.ca>
2013-02-14 12:49:36 -0500

    * updated a2q4.m

--- A2/m/a2q4.m
+++ A2/m/a2q4.m
@@ -2,10 +2,12 @@ g = 9.81;
 m = 90;
 c = 0.72;
 s = 0.5;
-[g m c s] = {1 3 4 5}{:};
 options = odeset('Events',@hit_ground);
-[t Z] = ode45(@(t,z) odefunc(t,z,1.29,g,m,c,s),[0:0.05:5], [0,0,pi/8,10],options);
-distance_b = Z(length(Z),1)
-[t Z] = ode45(@(t,z) odefunc(t,z,0.94,g,m,c,s),[0:0.05:5], [0,0,pi/8,10],options);
+domain = [0:0.05:5];
+initial = [0,0,pi/8,10];
+[t Z] = ode45(@(t,z) odefunc(t,z,0.94,g,m,c,s), domain, initial, options);
 distance_a = Z(length(Z),1)
-plot(t,Z);
+plot(t,Z); pause;
+[t Z] = ode45(@(t,z) odefunc(t,z,1.29,g,m,c,s), domain, initial, options);
+distance_b = Z(length(Z),1)
+plot(t,Z); pause;
