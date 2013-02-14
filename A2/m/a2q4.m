6b35005f570f800774238824520a1d2ccf5b9bf5
Alfred Chan <alfred8844@gmail.com>
2013-02-14 12:39:30 -0500

    * updated odefunc

--- A2/m/a2q4.m
+++ A2/m/a2q4.m
@@ -1,6 +1,11 @@
-options = odeset('Events',@events,'Refine',2);
-[t Z] = ode45(@(t,z) odefunc(t,z,1.29),[0:0.05:5], [0,0,pi/8,10],options);
+g = 9.81;
+m = 90;
+c = 0.72;
+s = 0.5;
+[g m c s] = {1 3 4 5}{:};
+options = odeset('Events',@hit_ground);
+[t Z] = ode45(@(t,z) odefunc(t,z,1.29,g,m,c,s),[0:0.05:5], [0,0,pi/8,10],options);
 distance_b = Z(length(Z),1)
-[t Z] = ode45(@(t,z) odefunc(t,z,0.94),[0:0.05:5], [0,0,pi/8,10],options);
+[t Z] = ode45(@(t,z) odefunc(t,z,0.94,g,m,c,s),[0:0.05:5], [0,0,pi/8,10],options);
 distance_a = Z(length(Z),1)
 plot(t,Z);
--- A2/m/events.m
+++
@@ -1,9 +0,0 @@
-function [values,halt,dir] = events(t,z)
-	values = zeros(1,1);
-	values(1) = z(2);
-	halt = zeros(1,1);
-	dir = zeros(1,1);
-	if (z(2) <= 0)
-		halt(1) = 1;
-	end
-endfunction
