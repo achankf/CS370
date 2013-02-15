8c7648bf94cfe2aa797a09a00949d9eac0e270bb
Alfred Chan <alfred8844@gmail.com>
2013-02-15 15:52:58 -0500

    * final fix

--- A2/m/a2q4.m
+++ A2/m/a2q4.m
@@ -3,9 +3,12 @@ domain = [0:0.05:5];
 initial = [0,0,pi/8,10];
 
 [t Z] = ode45(@(t,z) sprinter(z,0.94), domain, initial, options);
+animation(t,Z);
 distance_a = Z(length(Z),1)
 [t Z] = ode45(@(t,z) sprinter(z,1.29), domain, initial, options);
+animation(t,Z);
 distance_b = Z(length(Z),1)
 v0 = fzerotx('sprinter_root',[-10,100])
 [t Z] = ode45(@(t,z) sprinter(z,1.29), domain, [0,0,pi/8,v0], options);
+animation(t,Z);
 distance_d = Z(length(Z),1)
--- A2/m/a2q4_eps.m4
+++ A2/m/a2q4_eps.m4
@@ -4,11 +4,9 @@ domain = [0:0.05:5];
 initial = [0,0,pi/8,10];
 
 [t Z] = ode45(@(t,z) sprinter(z,0.94), domain, initial, options);
-distance_a = Z(length(Z),1)
-plot(t,Z);
+plot(Z(:,1),Z(:,2));
 print -deps grapha.eps
 
 [t Z] = ode45(@(t,z) sprinter(z,1.29), domain, initial, options);
-distance_b = Z(length(Z),1)
-plot(t,Z);
+plot(Z(:,1),Z(:,2));
 print -deps graphb.eps
