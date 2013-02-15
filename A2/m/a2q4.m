ef54c9d57dfc41165d762530c42a62a51c0b8e05
a77chan <a77chan@uwaterloo.ca>
2013-02-14 19:13:44 -0500

    not much done

--- A2/m/a2q4.m
+++ A2/m/a2q4.m
@@ -4,14 +4,8 @@ initial = [0,0,pi/8,10];
 
 [t Z] = ode45(@(t,z) sprinter(z,0.94), domain, initial, options);
 distance_a = Z(length(Z),1)
-plot(t,Z);
-pause;
 [t Z] = ode45(@(t,z) sprinter(z,1.29), domain, initial, options);
-plot(t,Z);
-pause;
 distance_b = Z(length(Z),1)
 v0_c = fzerotx('sprinter_root',[-10,10])
 [t Z] = ode45(@(t,z) sprinter(z,1.29), domain, [0,0,pi/8,v0_c], options);
 distance_d = Z(length(Z),1)
-pause;
-plot(t,Z);
