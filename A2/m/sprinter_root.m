3c8959f6bf2736fbd49a2be5df26a74584944c18
a77chan <a77chan@uwaterloo.ca>
2013-02-14 20:41:43 -0500

    * sprinter_root WORKING!!!

--- A2/m/sprinter_root.m
+++ A2/m/sprinter_root.m
@@ -1,3 +1,4 @@
-function xdistance = sprinter_root(v)
-	temp = sprinter_de(pi/8,v, 0.94) - 8.9;
-	xdistance = temp(1);
+function distancex = sprinter_root(v)
+	options = odeset('Events',@hit_ground);
+	[t Z] = ode45(@(t,z) sprinter(z,0.94), [0:0.05:5], [0,0,pi/8,v],options);
+	distancex = Z(length(t),1) - 8.9;
