b444d5d987f197adbf154e31e90da9c6313041f5
Alfred Chan <a77chan@uwaterloo.ca>
2013-03-11 20:03:19 -0400

    * experiment

--- A3/m/compress.m
+++ A3/m/compress.m
@@ -1,7 +1,15 @@
 function [Y drop] = compress(f, tol)
-	F = blkproc(f, [8 8], @fft2);
-	drop_original = nnz(F);
-	F = blkproc(F, [8 8], @drop_small, tol);
-	drop_compressed = nnz(F);
-	Y = blkproc(F, [8 8], @revert);
-	drop =  drop_compressed / drop_original
+	[W H] = size(f);
+	num_drop = 0;
+	num_nz_coe = 0;
+	for i= 1:8:W
+		for j = 1:8:H
+			temp = fft2(f(i:i+7, j:j+7));
+			num_nz_coe = num_nz_coe + nnz(temp);
+			[temp num_dropped] = drop_small(temp, tol);
+			num_drop = num_drop + num_dropped;
+			Y(i:i+7, j:j+7) = uint8(real(ifft2(temp)));
+		end
+	end
+
+	drop =0;
--- A3/m/drop_small.m
+++ A3/m/drop_small.m
@@ -1,11 +1,14 @@
-function [F] = drop_small(F, tol)
+function [F num_dropped] = drop_small(F, tol)
 	%F1 = real(ifft2(F));
+	num_dropped = 0;
 	F1 = abs(real(F));
 	limit = max(F1(:)) * tol;
 	for i = [1:8]
 		for j = [1:8]
+c = abs(F(i,j)) < limit;
 			if (abs(F(i,j)) < limit)
 				F(i,j) = 0;
+				num_dropped = num_dropped + 1;
 			end
 		end
 	end
