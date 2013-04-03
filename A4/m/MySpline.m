d81d41c75ab706a5f5913539ad0f7b814bedb0c5
Alfred Chan <alfred8844@gmail.com>
2013-04-03 12:34:27 -0400

    progress on q2

---
+++ A4/m/MySpline.m
@@ -0,0 +1,49 @@
+function [a b c d] = MySpline(x,y)
+	n = length(x);
+	deltax = zeros(n:1);
+	a = zeros(n-1,1);
+	b = zeros(n-1,1);
+	c = zeros(n-1,1);
+	d = zeros(n-1,1);
+	y1 = zeros(n-1,1);
+
+	T = zeros(n,n);
+	r = zeros(n,1);
+
+	for i = 1:n-1
+		deltax(i) = x(i+1) - x(i);
+	end
+
+	for i = 1:n-1
+		y1(i) = (y(i+1) - y(i)) / deltax(i);
+	end
+
+	T(1,1) = 1;
+	T(1,n) = -1;
+	T(n,1) = 2 * deltax(n-1);
+	T(n,2) = deltax(n-1);
+	T(n,n-1) = 2 * deltax(1);
+	T(n,n) = deltax(1);
+
+	for i = 2:n-1
+		T(i,i-1) = deltax(i);
+		T(i,i) = 2 * (deltax(i-1) + deltax(i));
+		T(i,i+1) = deltax(i-1);
+	end
+
+	r(1) = 0;
+	for i = 2:n-1
+		y1_i_1 = (y(i) - y(i-1)) / deltax(i);
+		y1_i = (y(i+1) - y(i)) / deltax(i);
+		r(i) = 3 * (deltax(i) * y1(i-1)  + deltax(i-1) * y1(i));
+	end
+
+	s = bslashtx(T,r);
+
+	a = y([1:n-1],:);
+	b = s([1:n-1],:);
+
+	for i = 1:n-1
+		c(i) = (3 * y1(i) - 2 * s(i) - s(i+1)) / deltax(i);
+		d(i) = (s(i+1) + s(i) - 2 * y1(i)) / deltax(i)^2;
+	end
