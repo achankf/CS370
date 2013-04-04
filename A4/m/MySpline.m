0c61f0d8cf4e4d2674dbd5abe79f06556371fe79
Alfred Chan <alfred8844@gmail.com>
2013-04-04 00:39:06 -0400

    vectorized Q2; added comments

--- A4/m/MySpline.m
+++ A4/m/MySpline.m
@@ -1,24 +1,30 @@
 function [a b c d] = MySpline(x,y)
 
+	% initialize variables
 	n = length(x);
+	dx = diff(x);
+	y1 = diff(y) ./ dx(1:n-1);
 
+	% allocate memory
 	T = zeros(n-1);
 	r = zeros(n-1,1);
 
-	dx = diff(x');
-	y1 = diff(y) ./ dx(1:n-1);
-
-	T(1,[1,2,n-1]) = [2 * (dx(n-1) - dx(1)); -dx(n-1); dx(1)];
+	% generate the matrix T
+	T(1,[1,2,n-1]) = [2 * (dx(n-1) + dx(1)); -dx(n-1); dx(1)];
 	for i = 2:n-2
 		T(i,i-1:i+1) = [dx(i); 2 * (dx(i-1) + dx(i)); dx(i-1)];
 	end
-	T(n-1,[1,n-2,n-1]) = [-dx(n-2); dx(n-1); 2 * (dx(n-2) + dx(n-1))];
+	T(n-1,[1,n-2,n-1]) = [dx(n-2); dx(n-1); 2 * (dx(n-2) + dx(n-1))];
 
+	% generate r
 	r(1) = 3 * (dx(n-1) * y1(1) + dx(1) * y1(n-1));
 	r(2:n-1) = 3 * (dx(2:n-1) .* y1(1:n-2) + dx(1:n-2) .* y1(2:n-1));
 
+	% solve for Ts = r
 	s = bslashtx(T,r);
-	s(n) = s(1);
+	s(n) = s(1); % boundary condition
+
+	% generate coeficients
 	a = y([1:n-1],:);
 	b = s([1:n-1],:);
 	c = (3 * y1(1:n-1) - 2 * s(1:n-1) - s(2:n)) ./ dx(1:n-1);
--- A4/m/Q2.m
+++ A4/m/Q2.m
@@ -2,29 +2,38 @@ x = [0.489133 0.228816 0.358974 0.167277 0.399023 0.099878 0.431990 0.425153 0.5
 y = [0.941636 0.719170 0.716972 0.486447 0.487668 0.260562 0.272283 0.059096 0.054457 0.275702 0.266422 0.492308 0.476190 0.698657 0.705495 0.941636]';
 
 R = length(x);
-t = zeros(1, R);
 
-for p = 1:R-1
-	t(p+1) = t(p) + sqrt((x(p) - x(p+1))^2 + (y(p)-y(p+1))^2);
-end
+% create t
+t(2:R-1) = t(1:R-2) + sqrt(diff(x(1:R-1)).^2 + diff(y(1:R-1)).^2);
 
-fac=10;
-
+% create splines
 [xa xb xc xd] = MySpline(t,x);
 [ya yb yc yd] = MySpline(t,y);
 
-N = length(t);
-tref = zeros(1, fac*(N-1) + 1);
-for k = 1:N-1
+% refine t
+fac=10;
+tref = zeros(1, fac*(R-1) + 1);
+for k = 1:R-1
 	i = fac*(k-1)+1;
 	dt = t(k+1)-t(k);
 	for l = 0:fac-1
 		tref(i+l) = t(k) + l*dt/fac;
 	end
 end
-tref(fac*(N-1)+1) = t(N);
+tref(fac*(R-1)+1) = t(R);
 
+% evaluate splines
 tx = pwCEval(xa, xb, xc, xd, t, tref);
 ty = pwCEval(ya, yb, yc, yd, t, tref);
 
+fig = figure(1);
+subplot(1,2,1);
+plot(x,y,x,y,'*');
+title('The Original Polygon');
+axis off;
+subplot(1,2,2);
 plot(tx,ty);
+axis off;
+title('A Smooth Curve Using MySpline');
+
+saveas(fig, 'plots.png', 'png');
