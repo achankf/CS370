1331f0c0a93467e7151bbc83b75342b8781ec464
Alfred Chan <alfred8844@gmail.com>
2013-04-03 23:43:43 -0400

    vectorized as much as possible

--- A4/m/MySpline.m
+++ A4/m/MySpline.m
@@ -1,58 +1,25 @@
 function [a b c d] = MySpline(x,y)
-%	A = sortrows([x0' y0]);
-%	x = A(:,1);
-%	y = A(:,2);
 
 	n = length(x);
 
-	deltax = zeros(n:1);
-	y1 = zeros(n-1,1);
-	T = zeros(n,n);
-	r = zeros(n,1);
+	T = zeros(n-1);
+	r = zeros(n-1,1);
 
-	for i = 1:n-1
-		deltax(i) = x(i+1) - x(i);
+	dx = diff(x');
+	y1 = diff(y) ./ dx(1:n-1);
+
+	T(1,[1,2,n-1]) = [2 * (dx(n-1) - dx(1)); -dx(n-1); dx(1)];
+	for i = 2:n-2
+		T(i,i-1:i+1) = [dx(i); 2 * (dx(i-1) + dx(i)); dx(i-1)];
 	end
+	T(n-1,[1,n-2,n-1]) = [-dx(n-2); dx(n-1); 2 * (dx(n-2) + dx(n-1))];
 
-	for i = 1:n-1
-		y1(i) = (y(i+1) - y(i)) / deltax(i);
-	end
-
-%	T(1,1) = 1;
-%	T(1,n) = -1;
-%	T(n,1) = 2 * deltax(n-1);
-%	T(n,2) = deltax(n-1);
-%	T(n,n-1) = 2 * deltax(1);
-%	T(n,n) = deltax(1);
-
-	T(1,1)=1;
-	T(1,2) = 1/2;
-	T(n,n) = 1;
-	T(n,n-1) = 1/2;
-	for i = 2:n-1
-		T(i,i-1) = deltax(i);
-		T(i,i) = 2 * (deltax(i-1) + deltax(i));
-		T(i,i+1) = deltax(i-1);
-	end
-
-	%r(1) = 0;
-	for i = 2:n-1
-		y1_i_1 = (y(i) - y(i-1)) / deltax(i);
-		y1_i = (y(i+1) - y(i)) / deltax(i);
-		r(i) = 3 * (deltax(i) * y1(i-1)  + deltax(i-1) * y1(i));
-	end
-	r(1) = 3/2 * y1(1);
-	r(n) = 3/2 * y1(n-1);
+	r(1) = 3 * (dx(n-1) * y1(1) + dx(1) * y1(n-1));
+	r(2:n-1) = 3 * (dx(2:n-1) .* y1(1:n-2) + dx(1:n-2) .* y1(2:n-1));
 
 	s = bslashtx(T,r);
-
+	s(n) = s(1);
 	a = y([1:n-1],:);
 	b = s([1:n-1],:);
-
-	c = zeros(n-1,1);
-	d = zeros(n-1,1);
-
-	for i = 1:n-1
-		c(i) = (3 * y1(i) - 2 * s(i) - s(i+1)) / deltax(i);
-		d(i) = (s(i+1) + s(i) - 2 * y1(i)) / deltax(i)^2;
-	end
+	c = (3 * y1(1:n-1) - 2 * s(1:n-1) - s(2:n)) ./ dx(1:n-1);
+	d = (s(2:n) + s(1:n-1) - 2 * y1(1:n-1)) ./ (dx(1:n-1).^2);
